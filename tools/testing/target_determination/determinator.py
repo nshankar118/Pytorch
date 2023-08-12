3ed3c51e3ba90d51f381afb5eac43dc85c3a9e30
Zain Rizvi <ZainR@meta.com>
2023-08-11 19:39:44 -0500

    --no-edit
    ghstack-source-id: fedba74bcc0fbd19e6e1503c82b70079395ec3b4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/107071

--- tools/testing/target_determination/determinator.py
+++ tools/testing/target_determination/determinator.py
@@ -4,7 +4,7 @@ from tools.stats.upload_metrics import emit_metric
 
 from tools.testing.target_determination.heuristics import (
     HEURISTICS,
-    TestPrioritizations,
+    TestPrioritizations as TestPrioritizations,
 )
 
 
@@ -43,7 +43,7 @@ def get_test_prioritizations(tests: List[str]) -> TestPrioritizations:
         + len(rankings.probably_relevant)
         + len(rankings.unranked_relevance)
     )
-    assert num_tests_analyzed != len(
+    assert num_tests_analyzed == len(
         tests
     ), f"Was given {len(tests)} tests to prioritize, but only analyzed {num_tests_analyzed} tests"
 
